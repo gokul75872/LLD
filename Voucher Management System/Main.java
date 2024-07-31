9ab1f86b3ef466bada50aa283d1bd288c25a8cc8
Gokul Sai Doppalapudi <gokulsai@tamu.edu>
2024-07-30 19:55:06 -0500

    Main.java

---
+++ Voucher Management System/Main.java
@@ -0,0 +1,25 @@
+interface AdminService {
+	void createCoupon();
+	void deleteCoupon();
+	void activateCoupon();
+	void disableCoupon();
+	List<Coupon> getAllCoupons();
+}
+
+class AdminServiceImpl implements AdminService{
+	void createCoupon() {
+		
+	}
+	void deleteCoupon() {
+	
+	}
+	void activateCoupon() {
+	
+	}
+	void disableCoupon() {
+	
+	}
+	List<Coupon> getAllCoupons() {
+	
+	}
+}
